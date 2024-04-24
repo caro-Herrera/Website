f7c8dbf814e687e296195a6d4a6999294960f120
caro-Herrera <ana.herrera@edu.sait.ca>
2024-04-23 22:31:12 -0600

    first commit

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+(function(){
+    const openButton = document.querySelector('.nav_menu');
+    const menu = document.querySelector('.nav_link');
+    const closeMenu = document.querySelector('.nav_close');
+
+    openButton.addEventListener('click',()=>{
+        menu.classList.add('nav__link--show');
+    });
+
+    closeMenu.addEventListener('click', ()=>{
+        menu.classList.remove('nav__link--show');
+    });
+    
+})();
